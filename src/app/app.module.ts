65acd95aa475e55690c41a7c8a982b3d6637d32f
AmalAlby <amalalbyn@gmail.com>
2022-11-29 09:52:00 +0530

    passenger view created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PassengerdataComponent } from './passengerdata/passengerdata.component';
+
+
+const myroute:Routes=[
+
+    {
+      path:"",
+      component:PassengerdataComponent
+    }
+
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PassengerdataComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myroute)
   ],
   providers: [],
   bootstrap: [AppComponent]
